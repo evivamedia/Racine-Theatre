7679d33382d2092bd37e183acc6af081292f9494
Eviva Media <admin@evivamedia.com>
2016-03-09 11:41:05 +0800

    FONT enque

--- Builder-RacineTheatre/functions.php
+++ Builder-RacineTheatre/functions.php
@@ -11,3 +11,29 @@ add_theme_support( 'builder-3.0' );
 
 // Add support for responsive features
 add_theme_support( 'builder-responsive' );
+
+
+/**
+ * Enqueue scripts and styles
+ */
+function enqeue_theme() {
+	$bootstrap_version = '3.3.6';
+	$fontawesome_version = '4.5.0';
+
+	wp_enqueue_script( 'bootstrap-min-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/'.$bootstrap_version.'/js/bootstrap.min.js', array(), $bootstrap_version, true );
+
+	wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/'.$bootstrap_version.'/css/bootstrap.min.css' );
+
+	wp_enqueue_style( 'bootstrap_more_icons', '//netdna.bootstrapcdn.com/font-awesome/'.$fontawesome_version.'/css/font-awesome.css' );
+
+	
+
+	wp_enqueue_style( 'montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+	
+	wp_enqueue_style( 'theme_css', get_template_directory_uri() .'-RacineTheatre/style-font.css');
+	wp_enqueue_style( 'theme_css', get_template_directory_uri() .'-RacineTheatre/style-custom.css');
+	
+
+}
+add_action( 'wp_enqueue_scripts', 'enqeue_theme' );
