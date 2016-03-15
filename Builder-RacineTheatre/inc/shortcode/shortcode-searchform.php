ee02a474cc60c3e2227310b765e0c9992c46d81a
Eviva Media <admin@evivamedia.com>
2016-03-15 20:37:50 +0800

    added date search

--- Builder-RacineTheatre/inc/shortcode/shortcode-searchform.php
+++ Builder-RacineTheatre/inc/shortcode/shortcode-searchform.php
@@ -5,8 +5,9 @@
 function searchForm($atts){
 	$html ='';
 
-	$html .='<form role="search" method="get" id="production_searchform">';
+	$html .='<form role="search" method="get" id="production_searchform" action="'.esc_url( home_url( "/" ) ).'">';
 		$html .='<input type="text" value="" name="s" class="production_title" placeholder="ENTER TITLE OF SHOW">';
+		$html .='<input type="text" value="" name="d" class="production_date" id="prod_datepicker" placeholder="Month Year">';
 		$html .='<input type="submit" class="production_submit" value="Search">';
 	$html .='</form>';
 	return $html;
