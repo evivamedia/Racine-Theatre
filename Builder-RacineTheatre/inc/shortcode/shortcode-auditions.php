390807fa85a09e1ca6bc3d9950433e89bb0a65a6
Eviva Media <admin@evivamedia.com>
2016-03-16 16:17:01 +0800

    added function to get dates in audition

--- Builder-RacineTheatre/inc/shortcode/shortcode-auditions.php
+++ Builder-RacineTheatre/inc/shortcode/shortcode-auditions.php
@@ -44,16 +44,19 @@ function auditionlist_func($atts){
 		$html .='<div class="audition_wrapper">';
 			$html .='<div class="a_header"><u>'.$header.':</u></div>';
 			if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); 
-				$startDate = get_field('audition_start_date',$post->ID);
+			
+				$remark = get_field('remark',get_the_ID());
+				$startDate = get_field('audition_start_date',get_the_ID());
 				$startDate = new DateTime($startDate);
-				$endDate = get_field('audition_end_date',$post->ID);
+				$endDate = get_field('audition_end_date',get_the_ID());
 				$endDate = new DateTime($endDate);
+
 				$html .='<div class="audition_list_wrap">';
 					$html .='<div class="a_title"><strong>'.get_the_title().'</strong></div>';
 					$html .='<span>'.$startDate->format('M. d').'</span> - ';
 					$html .='<span>'.$endDate->format('M. d, Y').'</span> at ';
 					$html .='<span>'.$endDate->format('g a').'</span>';
-					$html .='<div class="a_excerpt">'.get_the_excerpt().'</div>';
+					$html .='<div class="a_excerpt">'.$remark.'</div>';
 					$html .='<div class="vc_btn3-container vc_btn3-center a_button"><a class="vc_general  vc_btn3 vc_btn3-size-xs vc_btn3-shape-square vc_btn3-style-outline vc_btn3-block vc_btn3-icon-left vc_btn3-color-black" href="'.get_the_permalink().'" title="" target="_self"><i class="vc_btn3-icon fa fa-plus"></i> AUDITION DETAILS</a></div>';
 				$html .='</div>';
 			endwhile; 
@@ -68,3 +71,19 @@ function auditionlist_func($atts){
 
 add_shortcode( 'auditionlist', 'auditionlist_func' );
 
+
+function audition_dates($ID){
+	$html ="";
+
+	$startDate = get_field('audition_start_date',$ID);
+	$startDate = new DateTime($startDate);
+	$endDate = get_field('audition_end_date',$ID);
+	$endDate = new DateTime($endDate);
+
+	$html .='<span>'.$startDate->format('M. d').'</span> - ';
+	$html .='<span>'.$endDate->format('M. d, Y').'</span> at ';
+	$html .='<span>'.$endDate->format('g a').'</span>';
+
+	return $html;
+
+}
