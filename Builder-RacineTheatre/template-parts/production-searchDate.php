0ee39b31f716966b7325db9ed5f0c36b86a94607
Eviva Media <admin@evivamedia.com>
2016-03-16 16:20:17 +0800

    production search fixes

--- Builder-RacineTheatre/template-parts/production-searchDate.php
+++ Builder-RacineTheatre/template-parts/production-searchDate.php
@@ -1,5 +1,33 @@
+
 <?php 
 	$date = $_GET['d'];
 	$dateSearch = $date;
-	echo productions_func(array('date' => $dateSearch));
+	$output = productions_func(array('date' => $dateSearch));
 ?>
+
+<?php if($output !=""): ?>
+	<h2><strong>Production</strong> Result</h2>
+	<div class="prod_result-container">
+		<?php echo $output; ?>
+	</div>
+<?php else: ?>
+	<div class="no-results-msg">
+		<h3>Sorry, no results were found based on your search term.</h3>
+	</div>
+	
+	<!--PRODUCTION SEARCH-->
+	<section class="search_section light">
+		<div class="container">
+			<div class="vc_row wpb_row vc_row-fluid">
+				<div class="wpb_column vc_column_container vc_col-sm-12">
+					<div class="vc_column-inner ">
+						<div class="wpb_wrapper">
+							<?php echo do_shortcode('[productionsearchform]'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php endif; ?>
