35bf9d66aef3f74f3676ea1ec2a6ee1e646e5d35
Alipio Gabriel <alipio@evivamedia.com>
2016-03-22 18:37:49 +0800

    Fixed some bugs and added a production template function

--- Builder-RacineTheatre/inc/shortcode/shortcode-productions.php
+++ Builder-RacineTheatre/inc/shortcode/shortcode-productions.php
@@ -1,5 +1,55 @@
 <?php
 
+
+function prod_template($filter){
+global $wp_theatre;
+$html .='<section class="upcoming_productions_row productions_row sameheight vc_row wpb_row vc_row-fluid">';
+$count = 0;
+
+foreach ($wp_theatre->productions -> get($filter) as $production) {
+	$productionID = $production->ID;
+	$thumbnailID = $production->thumbnail();
+	$posterURL = wp_get_attachment_url($thumbnailID);	
+	$postersmallURL = get_field('production_small_thumbnail',$productionID);
+	$title  = $production->title();
+	$excerpt = $production->excerpt(array('words' => 20));
+	$dates = get_startDate($productionID,'F d').' - '.get_endDate($productionID,'F d');
+	$permalink = $production->permalink();
+	$mainticketURL = get_field('main_ticket_url',$productionID);
+	if(empty($mainticketURL)){$mainticketURL = $production->permalink(); $target="_self";}
+	$mainticketLABEL = get_field('main_ticket_label',$productionID);
+	if(empty($mainticketLABEL)){$mainticketLABEL = "Buy Ticket";}
+
+	$html .='<div class=" production_column wpb_column vc_column_container vc_col-md-3 vc_col-sm-6 vc_col-xs-12">';
+					$html .='<div class="vc_column-inner">';
+						$html .='<div class="production_wrapper">';
+							$html .='<div class="p_image"><img src="'.$postersmallURL.'" /></div>';
+							$html .='<div class="p_content">';
+								$html .='<div class="p_content_meta">';
+									$html .='<div class="p_title"><h4>'.$title.'</h4></div>';
+									$html .='<div class="p_excerpt">'.$excerpt.'</div>';
+								$html .='</div>';
+								$html .='<div class="p_info_meta">';
+									$html .='<div class="p_date">'.$dates.'</div>';
+									$html .='<div class="p_mi"><a href="'.$permalink.'">More Info</a></div>';
+									//$html .='<div class="p_ticketbutton"><a href="'.$mainticketURL.'" target="'.$target.'">'.$mainticketLABEL.'</a></div>';
+									$html .= prod_ticketbutton($productionID);
+								$html .='</div>';
+							$html .='</div>';
+						$html .='</div>';	
+					$html .='</div>';	
+	$html .='</div>';
+
+	$count++;
+	if($count %4 == 0):
+		$html .='<section class="upcoming_productions_row productions_row sameheight vc_row wpb_row vc_row-fluid">';
+	endif;
+}
+	$html .='</section>';
+	return $html;
+
+}
+
 function productions_func($atts){
 	$arg = shortcode_atts( array(
         'limit' => '',
@@ -53,62 +103,95 @@ function productions_func($atts){
 	//END SEARCH
 
 
+	$html = prod_template($filter);
+	return $html;
+
+}
+
+add_shortcode( 'productions', 'productions_func' );
+
+function upcomingproduction_func($atts){
+	$arg = shortcode_atts( array(
+        'limit' => ''
+    ), $atts );
+    $limit = $arg['limit'];
+    if (empty($limit)) {
+    	$limit = -1;
+    }
+
+	$filter = array('start' => 'now', 'limit'=> $limit );
+
+	$html = prod_template($filter);
+	return $html;
+
+}
+
+add_shortcode( 'upcomingproduction', 'upcomingproduction_func' );
+
+
+function pastproduction_func($atts){
+	$arg = shortcode_atts( array(
+        'limit' => ''
+    ), $atts );
+    $limit = $arg['limit'];
+    if (empty($limit)) {
+    	$limit = -1;
+    }
+
 	$productions = new WPT_Productions();
 	$events = new WPT_Events();
 
+	$filter = array('end' => 'now','limit'=> $limit );
+
 	$target="_blank";
-	$startDates = array();
-
+	$endDates = array();
+	$endDate="";
 	$html ='';
+	$html .='<section class="past_production productions_row vc_row-o-equal-height vc_row-o-content-bottom vc_row-flex vc_row wpb_row vc_row-fluid">';
 
-	$html .='<section class="productions_row sameheight vc_row wpb_row vc_row-fluid">';
-	$count = 0;
 		foreach ($productions -> get($filter) as $production) {
 			$productionID = $production->ID;
 			$thumbnailID = $production->thumbnail();
 			$posterURL = wp_get_attachment_url($thumbnailID);	
-			$postersmallURL = get_field('production_small_thumbnail',$productionID);
+			$production_banner = get_field('production_banner',$productionID);
+			if(!empty($production_banner)):
+				$postersmallURL = $production_banner;
+			else:
+				$postersmallURL = get_field('production_small_thumbnail',$productionID);
+			endif;
+
 			$mainticketURL = get_field('main_ticket_url',$productionID);
 			if(empty($mainticketURL)){$mainticketURL = $production->permalink(); $target="_self";}
 			$mainticketLABEL = get_field('main_ticket_label',$productionID);
 			if(empty($mainticketLABEL)){$mainticketLABEL = "Buy Ticket";}
 
-			$event_filter = array('production' => $production->ID);
-			foreach ($events -> get($event_filter) as $event) {
-					$startDates[] = $event->startdate();			
-			}
-				$html .='<div class="production_column wpb_column vc_column_container vc_col-md-3 vc_col-sm-6 vc_col-xs-12">';
+
+
+			//if(prod_ended($production->ID)):
+				$html .='<div class="production_column wpb_column vc_column_container vc_col-md-4 vc_col-sm-12 vc_col-xs-12" style="background:url('.$postersmallURL.') 50%;	;background-size:cover;">';
 					$html .='<div class="vc_column-inner">';
 						$html .='<div class="production_wrapper">';
-							$html .='<div class="p_image"><img src="'.$postersmallURL.'" /></div>';
-							$html .='<div class="p_content">';
+							$html .='<div class="p_content white-scheme">';
 								$html .='<div class="p_content_meta">';
 									$html .='<div class="p_title"><h4>'.$production->title().'</h4></div>';
-									$html .='<div class="p_excerpt">'.$production->excerpt().'</div>';
 								$html .='</div>';
 								$html .='<div class="p_info_meta">';
-									$html .='<div class="p_date">'.$production->dates().'</div>';
+								$endDate = get_endDate($productionID,'F Y');
+									if(!empty($endDate)){
+											$html .='<div class="p_date">'.$endDate.'</div>';
+									}	
 									$html .='<div class="p_mi"><a href="'.$production->permalink().'">More Info</a></div>';
-									//$html .='<div class="p_ticketbutton"><a href="'.$mainticketURL.'" target="'.$target.'">'.$mainticketLABEL.'</a></div>';
-									$html .= prod_ticketbutton($productionID);
 								$html .='</div>';
 							$html .='</div>';
 						$html .='</div>';	
 					$html .='</div>';	
 				$html .='</div>';
-		$count++;
+			//endif;
 		}
 
 	$html .='</section>';
-
-	if($count==0):
-		$html="";
-	endif;
 	return $html;
-	$count=0;
 }
-
-add_shortcode( 'productions', 'productions_func' );
 
 function get_production_categories_func(){
 	$productions = new WPT_Productions();
@@ -156,129 +239,40 @@ function get_currentseason(){
 	}
 }
 
-function upcomingproduction_func($atts){
-	$arg = shortcode_atts( array(
-        'limit' => ''
-    ), $atts );
-    $limit = $arg['limit'];
-    if (empty($limit)) {
-    	$limit = -1;
-    }
 
-	$filter = array('start' => 'now', 'limit'=> $limit );
-	$productions = new WPT_Productions();
-	$target="_blank";
-
-	$html ='';
-	$html .='<section class="productions_row sameheight vc_row wpb_row vc_row-fluid">';
-
-		foreach ($productions -> get($filter) as $production) {
-			$productionID = $production->ID;
-			$thumbnailID = $production->thumbnail();
-			$posterURL = wp_get_attachment_url($thumbnailID);	
-			$postersmallURL = get_field('production_small_thumbnail',$productionID);
-			$mainticketURL = get_field('main_ticket_url',$productionID);
-			if(empty($mainticketURL)){$mainticketURL = $production->permalink(); $target="_self";}
-			$mainticketLABEL = get_field('main_ticket_label',$productionID);
-			if(empty($mainticketLABEL)){$mainticketLABEL = "Buy Ticket";}
-
-
-			$html .='<div class="production_column wpb_column vc_column_container vc_col-md-3 vc_col-sm-6 vc_col-xs-12">';
-				$html .='<div class="vc_column-inner">';
-					$html .='<div class="production_wrapper">';
-						$html .='<div class="p_image"><img src="'.$postersmallURL.'" /></div>';
-						$html .='<div class="p_content">';
-							$html .='<div class="p_content_meta">';
-								$html .='<div class="p_title"><h4>'.$production->title().'</h4></div>';
-								$html .='<div class="p_excerpt">'.$production->excerpt().'</div>';
-							$html .='</div>';
-							$html .='<div class="p_info_meta">';
-								$html .='<div class="p_date">'.$production->dates().'</div>';
-								$html .='<div class="p_mi"><a href="'.$production->permalink().'">More Info</a></div>';
-								//$html .='<div class="p_ticketbutton"><a href="'.$mainticketURL.'" target="'.$target.'">'.$mainticketLABEL.'</a></div>';
-								$html .= prod_ticketbutton($productionID);
-							$html .='</div>';
-						$html .='</div>';
-					$html .='</div>';	
-				$html .='</div>';	
-			$html .='</div>';
-		}
-
-	$html .='</section>';
-
-	//print_r($production->get_events());
-	return $html;
-
-}
-
-add_shortcode( 'upcomingproduction', 'upcomingproduction_func' );
-
-
-function pastproduction_func($atts){
-	$arg = shortcode_atts( array(
-        'limit' => ''
-    ), $atts );
-    $limit = $arg['limit'];
-    if (empty($limit)) {
-    	$limit = -1;
-    }
-
-	$productions = new WPT_Productions();
-	$events = new WPT_Events();
-
-	$filter = array('end' => 'now','limit'=> $limit );
-
-	$target="_blank";
-	$endDates = array();
-	$endDate="";
-	$html ='';
-	$html .='<section class="past_production productions_row vc_row-o-equal-height vc_row-o-content-bottom vc_row-flex vc_row wpb_row vc_row-fluid">';
-
-		foreach ($productions -> get($filter) as $production) {
-			$productionID = $production->ID;
-			$thumbnailID = $production->thumbnail();
-			$posterURL = wp_get_attachment_url($thumbnailID);	
-			$postersmallURL = get_field('production_small_thumbnail',$productionID);
-			$mainticketURL = get_field('main_ticket_url',$productionID);
-			if(empty($mainticketURL)){$mainticketURL = $production->permalink(); $target="_self";}
-			$mainticketLABEL = get_field('main_ticket_label',$productionID);
-			if(empty($mainticketLABEL)){$mainticketLABEL = "Buy Ticket";}
-
-			$event_filter = array('production' => $production->ID);
-			foreach ($events -> get($event_filter) as $event) {
-				$endDates[] = $event->enddate();			
-			}
-
-			//if(prod_ended($production->ID)):
-				$html .='<div class="production_column wpb_column vc_column_container vc_col-md-4 vc_col-sm-12 vc_col-xs-12" style="background:url('.$postersmallURL.');background-size:cover;">';
-					$html .='<div class="vc_column-inner">';
-						$html .='<div class="production_wrapper">';
-							$html .='<div class="p_content white-scheme">';
-								$html .='<div class="p_content_meta">';
-									$html .='<div class="p_title"><h4>'.$production->title().'</h4></div>';
-								$html .='</div>';
-								$html .='<div class="p_info_meta">';
-									if(!empty($endDates)){
-											$endDate = new DateTime(end($endDates));
-											$endDate = $endDate->format('F Y');
-											$html .='<div class="p_date">'.$endDate.'</div>';
-									}	
-									$html .='<div class="p_mi"><a href="'.$production->permalink().'">More Info</a></div>';
-								$html .='</div>';
-							$html .='</div>';
-						$html .='</div>';	
-					$html .='</div>';	
-				$html .='</div>';
-			//endif;
-		}
-
-	$html .='</section>';
-
-	return $html;
-
-}
 
 add_shortcode( 'pastproduction', 'pastproduction_func' );
+
+function get_endDate($ID,$format){
+	global $wp_theatre;
+	$filter = array( 'production' => $ID );
+	$endDate = "";
+	foreach ($wp_theatre->productions->get($filter) as $production) {
+		$event_filter = array('production' => $ID);
+		foreach ($wp_theatre->events-> get($event_filter) as $event) {
+			$endDates[] = $event->enddate();	
+		}
+		$endDate = new DateTime(end($endDates));
+		$endDate = $endDate->format($format);		
+	}
+	return $endDate;
+}
+
+function get_startDate($ID,$format){
+	global $wp_theatre;
+	$filter = array( 'production' => $ID );
+	$startDate = "";
+	foreach ($wp_theatre->productions->get($filter) as $production) {
+		$event_filter = array('production' => $ID);
+		foreach ($wp_theatre->events-> get($event_filter) as $event) {
+			$startDates[] = $event->startdate();	
+		}
+		$startDate = new DateTime($startDates[0]);
+		$startDate = $startDate->format($format);		
+	}
+
+	return $startDate;
+}
 
 function prod_ended($productionID){
 	$events = new WPT_Events();
@@ -309,12 +303,14 @@ function prod_ticketbutton($productionID){
 	if(!prod_ended($productionID)):
 		foreach ($productions -> get($production_filter) as $production) {
 			$mainticketURL = get_field('main_ticket_url',$productionID);
+			$isShow = get_field('shows_flag',$productionID);
 			$target = "_BLANK";
-			if(!empty($mainticketURL)):
-				//if(empty($mainticketURL)){$mainticketURL = $production->permalink(); $target = "_SELF";}
-				$mainticketLABEL = get_field('main_ticket_label',$productionID);
-				if(empty($mainticketLABEL)){$mainticketLABEL = "Buy Ticket";}
-				$html .='<div class="p_ticketbutton"><a href="'.$mainticketURL.'" target="'.$target.'">'.$mainticketLABEL.'</a></div>';
+			$mainticketLABEL = get_field('main_ticket_label',$productionID);
+			if(empty($mainticketLABEL)){$mainticketLABEL = "Buy Ticket";}
+			if(!$isShow):
+				$html .='<div class="p_ticketbutton"><a href="'.$production->permalink().'">Buy Ticket</a></div>';
+			else:
+				$html .='<div class="p_ticketbutton"><a href="'.$production->permalink().'">See Shows</a></div>';
 			endif;
 		}
 	endif;
